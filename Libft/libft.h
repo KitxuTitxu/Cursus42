775187627fec72c81d6a767b49ecc41ca0e0de56
Kitxu <140657847+KitxuTitxu@users.noreply.github.com>
2023-12-14 11:57:03 +0100

    Add files via upload

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: iginsaus <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 09:25:17 by iginsaus          #+#    #+#             */
-/*   Updated: 2023/12/14 09:52:29 by iginsaus         ###   ########.fr       */
+/*   Updated: 2023/12/14 11:34:50 by iginsaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int		ft_isdigit(int c);//2-checks is a digit.
 int		ft_isalnum(int c);//3-checks is alphanumeric.
 int		ft_isascii(int c);//4-checks is ascii(0 to 127).
 int		ft_isprint(int c);//5-checks is printable.
-
+size_t	ft_strlen(const char *s);//6-length of the string.
+void	*ft_memset(void *b, int c, size_t len);//7-c value len bytes to *b.
 int		ft_atoi(const char *str);
 int		ft_lstsize(t_list *lst);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -41,9 +42,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-v
+void	ft_putstr_fd(char
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
